502a6aec1d996c2ce9cb560922e6c5554317d1e7
David Barreto <barretollano@gmail.com>
2016-12-05 16:50:54 -0500

    update

--- rebase-all.js
+++ rebase-all.js
@@ -4,57 +4,62 @@ const MASTER = 'master';
 
 Repository
   .open('./')
-  .then(repository => repository.getReferences(Reference.TYPE.LISTALL).then(references => { 
-    return { repository, references };
-  }))
-  .then(processReferences)
-  .then(({repository, masterAnnotatedCommit, branchesAnnotatedCommit}) => {
-    let doRebases = branchesAnnotatedCommit.map(branchAnnotatedCommit => {
-      return Rebase.init(
+  .then(getReferences)
+  .then(getAnnotatedCommitsFromReferences)
+  .then(doRebases)
+  .then(finishRebases)
+  .then(handleSuccess)
+  .catch(handleError);
+
+
+function getReferences(repository) {
+  return repository.getReferences(Reference.TYPE.LISTALL)
+    .then(references => ({ repository, references }));
+}
+
+function getAnnotatedCommitsFromReferences({repository, references}) {
+  let masterReference = references.find(reference => reference.shorthand() === MASTER);
+  let branchesReference = references.filter(reference => reference.shorthand !== MASTER)
+
+  let createMasterAnnotatedCommit = AnnotatedCommit.fromRef(repository, masterReference);
+  let createBranchesAnnotatedCommit = branchesReference.map(reference => (
+      AnnotatedCommit.fromRef(repository, reference)
+    )
+  );
+
+  return Promise.all([createMasterAnnotatedCommit, ...createBranchesAnnotatedCommit])
+    .then(annotatedCommits => {
+      return { 
         repository, 
-        branchAnnotatedCommit, 
-        masterAnnotatedCommit, 
-        masterAnnotatedCommit
-      );
-    })
-    let createSignature = Signature.default(repository);
-    return Promise.all([createSignature, ...doRebases]);
-  })
-  .then(([signature, ...rebases]) => {
-    rebases.map(rebase => {
-      rebase.finish(signature);
-    });
-    
-  })
-  .then(() => {
-    console.log('Rebase completed!');
-  })
-  .catch(error => {
-    throw new Error(`Rebase failed: ${error}`);
+        masterAnnotatedCommit: annotatedCommits[0],
+        branchesAnnotatedCommit: annotatedCommits.slice(1)
+      };
   });
+}
 
-
-
-
-
-  function processReferences({repository, references}) {
-
-    let masterReference = references.find(reference => reference.shorthand() === MASTER);
-    let branchesReference = references.filter(reference => reference.shorthand !== MASTER)
-
-    let createMasterAnnotatedCommit = AnnotatedCommit.fromRef(repository, masterReference);
-    let createBranchesAnnotatedCommit = branchesReference.map(reference => (
-        AnnotatedCommit.fromRef(repository, reference)
-      )
+function doRebases({repository, masterAnnotatedCommit, branchesAnnotatedCommit}) {
+  let initRebases = branchesAnnotatedCommit.map(branchAnnotatedCommit => {
+    return Rebase.init(
+      repository, 
+      branchAnnotatedCommit, 
+      masterAnnotatedCommit, 
+      masterAnnotatedCommit
     );
+  })
+  let createSignature = Signature.default(repository);
+  return Promise.all([createSignature, ...initRebases]);
+}
 
-    return Promise.all([createMasterAnnotatedCommit, ...createBranchesAnnotatedCommit])
-      .then(annotatedCommits => {
-        return { 
-          repository, 
-          masterAnnotatedCommit: annotatedCommits[0],
-          branchesAnnotatedCommit: annotatedCommits.slice(1)
-        };
-    });
-  };
+function finishRebases([signature, ...rebases]) {
+  rebases.map(rebase => {
+    rebase.finish(signature);
+  });
+}
 
+function handleSuccess() {
+  console.log('Rebase completed!');
+}
+
+function handleError(error) {
+  throw new Error(`Rebase failed: ${error}`);
+}
